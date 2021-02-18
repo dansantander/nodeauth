4ddc2f765a5b60c833530669070cf230f6f919ad
dansantander <dansantandr@gmail.com>
2021-02-18 10:40:35 -0600

    chore: check email uniqueness

--- routes/auth.js
+++ routes/auth.js
@@ -5,12 +5,14 @@ const { registerValidation } = require('../validation');
 
 router.post('/register', async (req, res, next) => {
 
-  // Data validation
-  // const validation = schema.validate(req.body);
-  // This returns an object, so we can destructure it
-  // so that we only get the error messages:
+  // const validation = schema.validate(req.body) returns an object
+  // so we can destructure it so that we only get the error messages:
   const { error } = registerValidation(req.body);
   if (error) return res.status(400).send(error.details[0].message);
+
+  // Check email uniqueness
+  const emailExistence = await User.findOne({ email: req.body.email });
+  if (emailExistence) return res.status(400).send("Email already exists")
   
   // User creation
   const user = new User({
